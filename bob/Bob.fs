4ea08adf566fa45038e6a59d66b7acd8f9550cfc
tdavila <Antonio.Jesus.Davila@dnvgl.com>
2018-09-06 08:08:54 +0200

    Commit 3 first exercises

---
+++ bob/Bob.fs
@@ -0,0 +1,22 @@
+﻿module Bob
+
+open System
+
+let isAlphabetic = String.exists(fun c -> Char.IsLetter(c))
+
+// Performance ?
+let isUpperCase  = String.forall(fun c -> not (Char.IsLetter(c)) || Char.IsUpper(c))
+let isUpperCase2 (a:string) = a.ToUpper() = a
+
+let isQuestion (a:string) = a.EndsWith("?")
+
+// Perfomance ?
+let isEmpty = String.forall(fun c -> Char.IsControl(c) || Char.IsWhiteSpace(c))
+let isEmpty2 (a:string) = a.Replace("\t","").Replace(" ","").Length = 0
+
+let response (input: string): string = match input.Trim() with
+                                        | a  when isEmpty a -> "Fine. Be that way!"
+                                        | a  when isAlphabetic a && isUpperCase a && isQuestion a -> "Calm down, I know what I'm doing!"
+                                        | a  when isAlphabetic a && isUpperCase a  -> "Whoa, chill out!"
+                                        | a  when isQuestion a -> "Sure." 
+                                        | _ -> "Whatever."
---
+++ leap/Leap.fs
@@ -0,0 +1,4 @@
+﻿module Leap
+
+let leapYear (year: int): bool = if year % 4 = 0 && (year % 100 <> 0 || year % 400 = 0) then true
+                                 else false  
