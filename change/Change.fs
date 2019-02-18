83c89fceaff769a9c43c1bb6430c54ad688e7505
tdavila <tndavila@gmail.com>
2019-02-19 00:35:57 +0100

    exercises

---
+++ change/Change.fs
@@ -0,0 +1,39 @@
+﻿module Change
+open System
+
+exception NotExactChangeError of unit
+
+
+
+let minimumCoin coins = coins 
+                         |> List.min
+
+let validCoins coins value = coins 
+                               |> List.sortBy(fun x -> value/x)
+                               |> List.filter(fun x -> value/x >= 1)
+                               |> List.indexed
+                               |> List.takeWhile(fun (i,x) -> i < 2 )
+                               |> List.map(fun (i,x) -> x)
+
+let rec changeSolverB (coins:int list) (value:int)  = 
+        value |> function 
+                 | 0 -> [Some([])]
+                 | z when z < 0 -> [None]
+                 | x -> (validCoins coins value) 
+                        |> List.map(fun coin -> (value - coin)
+                                                   |> changeSolverB coins 
+                                                   |> List.choose(fun x -> x)
+                                                   |> List.map(fun j -> Some(coin::j))
+                                   )
+                        |> List.collect(fun sm -> sm)
+                                                                    
+let findFewestCoins (coins:int list) target =    changeSolverB coins target
+                                                 |> List.filter(fun x -> x <> None)
+                                                 |> function
+                                                    | [] -> None
+                                                    | x ->  x |> List.sortBy(fun l -> l.Value.Length)
+                                                              |> List.head 
+                                                              |> function 
+                                                                 | None -> None
+                                                                 | Some x -> Some(x |> List.rev)
+                                                             
---
+++ change/old.fs
@@ -0,0 +1,63 @@
+﻿module old
+
+exception NotExactChangeError of unit
+
+
+
+let minimumCoin coins = coins 
+                         |> List.min
+
+let validCoins coins value = coins 
+                               |> List.filter(fun x -> x <= value)
+
+let maximumCoin coins value = coins 
+                               |> List.filter(fun x -> x <= value)
+                               |> List.tryLast
+
+let previousCoin coins value = coins
+                                |> List.filter(fun x -> x <= value)
+                                |> List.rev
+                                |> List.skip 1
+                                |> List.tryHead
+
+
+let rec changeSolver coins (value:int)  = 
+                                value |> function 
+                                         | 0 -> []
+                                         | x -> (maximumCoin coins value) 
+                                                 |> function 
+                                                     | None -> raise(NotExactChangeError())
+                                                     | Some coin -> (value - coin)
+                                                                 |> function 
+                                                                    | 0 -> [coin]
+                                                                    | y when y >= (minimumCoin coins) ->
+                                                                        let res = coin::(changeSolver coins (value-coin)) 
+                                                                        printfn "%s" (res.ToString())
+                                                                        res
+                                                                    | y -> 
+                                                                        let prev = previousCoin coins coin
+                                                                        prev |> function 
+                                                                                |Some pr -> let res = pr::(changeSolver coins (value-pr)) 
+                                                                                            printfn "%s" (res.ToString())
+                                                                                            res
+                                                                                | None -> raise(NotExactChangeError())
+                                                                    
+let runRule coins target =
+    try
+       Some ( (changeSolver coins target)|> List.rev )
+    with
+     | NotExactChangeError() ->
+             None
+ 
+let findFewestCoinsB (coins:int list) target =   [(runRule (coins |> List.skip 1 ) target);
+                                                 (runRule coins target) ;
+                                                 (runRule (coins |> List.rev |> List.skip 1 |> List.rev) target)
+                                                 ]
+                                                 |> List.filter(fun x -> x <> None)
+                                                 |> function
+                                                    | [] -> None
+                                                    | x ->  x |> List.sortBy(fun l -> l.Value.Length)
+                                                              |> List.head
+ 
+
+
